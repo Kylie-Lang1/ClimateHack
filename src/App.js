d7c38af889552cdcf14c3c97bf4097063b4e9de1
Kylie-Lang1 <kylielang@pursuit.org>
2023-04-22 16:25:29 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import NavBar from "./Components/NavBar";
+import Home from "./Pages/Home"
+import Survey from "./Pages/Survey"
+import Solutions from "./Pages/Solutions"
+import Actions from "./Pages/Action"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/survey" element={<Survey />} />
+            <Route path="/solutions" element={<Solutions />} />
+            <Route path="/actions" element={<Actions />} />
+          </Routes>
+        </main>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+    return (
+        <div className="nav">
+            <button>About</button>
+            <button>Take our Survey</button>
+            <button>Solutions</button>
+            <button>Take Action!</button>
+        </div>
+    );
+}
+
+export default NavBar;
