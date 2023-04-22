c1e19a4e263630132c762bcad3e6d5853d47a000
Kylie-Lang1 <kylielang@pursuit.org>
2023-04-22 17:54:14 -0400

    tailwindcss config

--- src/Pages/Survey.js
+++ src/Pages/Survey.js
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 function Survey() {
+    const [ question, setQuestion ] = useState()
+
+
     return (
         <div>
-            
+            <div>
+
+            </div>
         </div>
     );
 }
