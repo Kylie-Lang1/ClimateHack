6f090046b158fe5210e5362ac08aaf74e04e8cb5
Kylie-Lang1 <kylielang@pursuit.org>
2023-04-22 22:25:20 -0400

    added result table to solutions page

--- src/Pages/Solutions.js
+++ src/Pages/Solutions.js
@@ -1,7 +1,22 @@
-function Solutions(props) {
+function Solutions({ results }) {
     return (
         <div>
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Propmt</th>
+                        <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
