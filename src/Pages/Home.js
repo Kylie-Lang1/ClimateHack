277d4374bcbb499e3e1fb08fda35ea9c4d44467e
Josh Manuel <joshmanuel@microsoft.com>
2023-04-23 02:56:03 -0400

    adding about me page; adding no results view

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Home() {
     return (
-        <div>
-            
+        <div className='w-5/6 px-6 py-10 my-16 m-auto mb-2 text-center'>
+            <h1>Welcome to Carbon Füutprint!</h1>
+            <p>We are here to help you calculate your individual carbon footprint and provide solutions to reduce it.</p>
+            <Link to="/survey">
+                <button className="border-2 w-auto px-3 py-1 rounded-lg my-3 shadow-lg">Take the Survey</button>
+            </Link>
+            <h2>About us</h2>
+            <ul>
+                <li>
+                    Hi, I'm Kylie Lang and I'm here to tell you about my new climate change app.
+                    Climate change is one of the most pressing issues of our time. It's a global problem that affects us all, and it's up to us to take action and make a difference.
+                    That's why I created my climate change app. It's designed to help people learn more about the issue and take action to reduce their carbon footprint.
+                    The app provides users with easy-to-understand information about climate change, including the latest research and news. It also offers practical advice on how to reduce your carbon footprint, like switching to renewable energy sources, using public transportation, and reducing your consumption of single-use plastics.
+                    The app also includes a carbon calculator, so you can track your carbon emissions and see how your lifestyle choices are impacting the environment.
+                    Finally, the app provides users with a platform to connect with others who are passionate about climate change. You can join conversations, share your ideas, and find ways to get involved in the fight against climate change.
+                    So if you're looking for a way to make a difference and take action on climate change, my app is the perfect solution. Download it today and join the fight against climate change!
+                </li>
+            </ul>
         </div>
     );
 }
--- src/Pages/Solutions.js
+++ src/Pages/Solutions.js
@@ -1,34 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+
 function Solutions({ results }) {
+    const navigate = useNavigate()
+
     return (
-        <div>
-            {
-                results.length ?
-                    <>    
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>Propmt</th>
-                                    <th>Answer</th>
-                                    <th className="ml-3">Score</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    results.map((result) => {
-                                        return (
-                                            <tr key={result.id}>
-                                                <td>{result.question}</td>
-                                                <td className="text-center">{result.answer}</td>
-                                                <td className={result.score >= 0 ? 'text-green-600 text-right' : 'text-red-600 text-right'}>{result.score}</td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
-                    </>
-                        : null
-        }
+        <div className="w-2/3 m-auto border py-10 my-16">
+            { results.length
+                ? <>
+                    <table className="w-3/4 m-auto">
+                        <thead>
+                            <tr>
+                                <th >Prompt</th>
+                                <th>Answer</th>
+                                <th className="ml-3">Score</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                results.map((result) => {
+                                    return (
+                                        <tr key={result.id}>
+                                            <td>{result.question}</td>
+                                            <td className="text-center">{result.answer}</td>
+                                            <td className={result.score >= 0 ? 'text-green-600 text-right' : 'text-red-600 text-right'}>{result.score}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                    </table>
+                </>
+                : <div className="grid justify-items-center">
+                    <div className="grid justify-items-center">No user results found.</div>
+                    <button className="border-2 w-auto px-3 py-1 rounded-lg my-3 shadow-lg" onClick={() => {navigate('/survey')}}>Take the Survey</button>
+                </div>
+            }
         </div>
     );
 }
