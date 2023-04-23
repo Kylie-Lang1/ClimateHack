8566f67209fda202eb28d229a089365ca940f7d1
Kylie-Lang1 <kylielang@pursuit.org>
2023-04-23 01:26:54 -0400

    styling for nav bar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
-        <div className="nav">
-            <Link to="/" >About</Link>
-            <Link to="/survey">Take our Survey</Link>
-            <Link to="/solutions">Solutions</Link>
-            <Link to="/actions">Take Action!</Link>
+        <div className="py-8 flex bg-blue-200 text-gray-800 text-xl text-right">
+            <Link to="/" className="w-1/2">About</Link>
+            <Link to="/survey" className="w-1/4">Take our Survey</Link>
+            <Link to="/solutions" className="w-1/4">Solutions</Link>
+            <Link to="/actions" className="w-1/4">Take Action!</Link>
         </div>
     );
 }
